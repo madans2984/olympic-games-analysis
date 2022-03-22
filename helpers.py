dfc394470c34c83e5cdd727b4e564eb13c451b9b
Shree Madan <smadan@olin.edu>
2022-03-21 22:52:16 -0400

    adding cache

--- helpers.py
+++ helpers.py
@@ -4,8 +4,11 @@ from bs4 import BeautifulSoup # library to parse HTML documents
 # get the response in the form of html
 def table_scrape(url, index=0):
     """
-    url: string of website url
-    returns dataframe
+    This functions takes a string representing the url of a wikipedia article and returns the first table on the page. If there are multiple tables and the return needs to be modified the index is an integer which represents the index of the table which needs to be scarped.
+    
+    Args:
+        url: string representing the url of a wikipedia article
+        index: index of the table on the wikipedia page
     """
     wikiurl=url
     table_class="wikitable sortable jquery-tablesorter"
@@ -22,6 +25,8 @@ def table_scrape(url, index=0):
         print("Error: This table should not be scraped due to its status code.")
 
 def medal_clean(df, year):
+    """
+    """
     df.rename(columns = {"Gold" : f"Gold-{year}", "Silver" : f"Silver-{year}", "Bronze" : f"Bronze-{year}", "Total" : f"Total-{year}"}, inplace = True)
     df.drop(["Rank"], axis = 1, inplace = True)
     df = df[:-1]
