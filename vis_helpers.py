ebe3b92777bb495d4cb7e383f8a9dd7992feb5ee
Shree Madan <smadan@olin.edu>
2022-03-30 11:36:23 -0400

    adding cleaned data

--- vis_helpers.py
+++ vis_helpers.py
@@ -1,9 +1,20 @@
-def medals_plot(df, year, type, medal):
+from ast import Index
+
+
+def medals_plot(df, type, medal):
+    import plotly.express as px
+    if type == "GDP":
+        fig = px.scatter(df, x = df["GDP"], y = df[f"{medal}"], trendline="ols", labels={f"{type}" : "GDP (per capita) in dollars", f"{medal}" : f"{medal} Olympic medals"}, title=f"{type} vs {medal} medals", hover_data=["Country", "Year"], log_x=False, color = "Year")
+    else:
+        fig = px.scatter(df, x = df[f"{type}"], y = df[f"{medal}"], trendline="ols", labels={f"{type}" : "Population (in thousands)", f"{medal}" : f"{medal} Olympic medals"}, title=f"{type} vs {medal} medals", hover_data=["Country", "Year"], log_x=True, color = "Year")
+    fig.show()
+
+def medals_plot_year(df, year, type, medal):
     """
     """
     import plotly.express as px
     if type == "GDP":
-        fig = px.scatter(df, x = df[f"{type}-{year}"], y = df[f"{medal}-{year}"], trendline="ols", labels={f"{type}-{year}" : "GDP (per capita) in dollars", f"{medal}-{year}" : f"{medal} Olympic medals"}, title=f"{type} vs {medal} medals in {year}", hover_data=['Country'], log_x=True)
+        fig = px.scatter(df, x = df[f"{type}-{year}"], y = df[f"{medal}-{year}"], trendline="ols", labels={f"{type}-{year}" : "GDP (per capita) in dollars", f"{medal}-{year}" : f"{medal} Olympic medals"}, title=f"{type} vs {medal} medals in {year}", hover_data=['Country'], log_x=False)
     else:
         fig = px.scatter(df, x = df[f"{type}-{year}"], y = df[f"{medal}-{year}"], trendline="ols", labels={f"{type}-{year}" : "Population (in thousands)", f"{medal}-{year}" : f"{medal} Olympic medals"}, title=f"{type} vs {medal} medals in {year}", hover_data=['Country'], log_x=True)
     fig.show()
