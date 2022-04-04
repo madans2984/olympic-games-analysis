c29e3e95fb0a6dd09f3915977bcd2665247b4931
Jules Brettle <jbrettle@olin.edu>
2022-04-04 10:00:08 -0400

    Run pylint and autopep8 on .py files

--- test_helpers.py
+++ test_helpers.py
@@ -1,3 +1,7 @@
+"""
+Cases and functions for testing the non- scraping and plotting functions in the
+helpers.py file
+"""
 import pytest
 import pandas as pd
 
@@ -12,6 +16,7 @@ clean_gdp_data_cases = [
     ("test_data/gdp_test_data2_raw.csv", "test_data/gdp_test_data2_clean.csv")
 ]
 
+
 @pytest.mark.parametrize("raw,clean", clean_gdp_data_cases)
 def test_clean_gdp_data(raw, clean):
     df_clean = pd.read_csv(clean)
@@ -22,6 +27,7 @@ clean_pop_data_cases = [
     ("test_data/pop_test_data1_raw.csv", "test_data/pop_test_data1_clean.csv"),
     ("test_data/pop_test_data2_raw.csv", "test_data/pop_test_data2_clean.csv")
 ]
+
 
 @pytest.mark.parametrize("raw,clean", clean_pop_data_cases)
 def test_clean_population_data(raw, clean):
@@ -34,4 +40,5 @@ def test_merge_dataframe():
     test_medals = pd.read_csv("test_data/medals_test_data_clean.csv")
     test_gdp = pd.read_csv("test_data/gdp_test_data1_clean.csv")
     test_pop = pd.read_csv("test_data/pop_test_data1_clean.csv")
-    assert df_merged.equals(merge_dataframes([test_medals, test_gdp, test_pop]))
+    assert df_merged.equals(merge_dataframes(
+        [test_medals, test_gdp, test_pop]))
