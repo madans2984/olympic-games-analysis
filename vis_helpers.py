a102611d4b918c3e5805d18f81d588bb08a076a8
Shree Madan <smadan@olin.edu>
2022-04-04 08:17:25 -0400

    adding new functions

--- vis_helpers.py
+++ vis_helpers.py
@@ -71,3 +71,58 @@ def model_check(data_frame, equation):
         mod = smf.ols(formula=f"{equation}", data=data_frame)
         res = mod.fit()
         print(res.summary())
+
+def average_medals_plot(df, sort, medal):
+    if sort == "Average GDP":
+        fig = px.scatter(
+                         df,
+                         x = df["Average GDP"],
+                         y = df[f"{medal}"],
+                         trendline="ols",
+                         labels={f"{sort}" :"Average GDP (per capita) in dollars",
+                                 f"{medal}" : f"{medal} Olympic medals"},
+                         title=f"Average GDP (per capita) vs {medal} medals from 2004-2016",
+                         hover_data=["Country"],
+                         log_x=False,
+                        )
+    elif sort == "Average Pop":
+        fig = px.scatter(
+                         df,
+                         x = df["Average Pop"],
+                         y = df[f"{medal}"],
+                         trendline="ols",
+                         labels={f"{sort}" : "Average Population from 2005-2015",
+                                 f"{medal}" : f"{medal} Olympic medals"},
+                         title=f"Average Population vs {medal} medals from 2004-2016",
+                         hover_data=["Country"],
+                         log_x=True,
+                        )
+    else:
+        fig = px.scatter(
+                 df,
+                 x = df["Average Athletes"],
+                 y = df[f"{medal}"],
+                 trendline="ols",
+                 labels={"Athletes" :"Average Total Number of Competitors",
+                 f"{medal}" : f"{medal} Number of Medals"},
+                 title=f"Average Number of Competitors vs Average Number of {medal} Medals",
+                 hover_data=["Country"],
+                 log_x=True
+                )    
+    fig.show()
+
+def average_context_plot(df, sort1="Average GDP", sort2="Average Pop"):
+    """
+    """
+    fig = px.scatter(
+                     df,
+                     x = df[f"{sort2}"],
+                     y = df[f"{sort1}"],
+                     trendline="ols",
+                     labels={f"{sort1}" : "Average GDP (per capita) in dollars",
+                             f"{sort2}" : "Average Population from 2005-2015"},
+                     title=f"Average GDP (per capita) vs Average Population from 2004-2016",
+                     hover_data=["Country"],
+                     log_x=True,
+                    )
+    fig.show()
